a80b57571309ed72bc995ecd5a92001947a7d177
Tyler Huffman <ty.huff24@gmail.com>
2020-07-29 17:54:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,4 @@
-def reverse_each_word(string)
-  array = string.split(" ")
-  reverse_array = []
-  array.each do |word|
-    reverse_array.push(word.reverse)
-  end
-  reverse_array.join(" ")
-end
+
 
 def reverse_each_word(string)
   array = string.split(" ")
